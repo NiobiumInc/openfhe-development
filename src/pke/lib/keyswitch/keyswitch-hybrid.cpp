8f821e7faa756f3d8dfabe683c0f2fb35e21b2ca
Brian Shirai <brian@niobiummicrosystems.com>
2025-04-17 10:47:30 -0700

    Track RNS format.

--- src/pke/lib/keyswitch/keyswitch-hybrid.cpp
+++ src/pke/lib/keyswitch/keyswitch-hybrid.cpp
@@ -136,7 +136,7 @@ EvalKey<DCRTPoly> KeySwitchHYBRID::KeySwitchGenInternal(const PrivateKey<DCRTPol
     for (const auto& c : a) {
       for (const auto& p : c.GetAllElements()) {
         p.CopyValues(openfhe_cprobe_address(p.GetId()));
-        openfhe_cprobe_key(p.GetId());
+        openfhe_cprobe_key(p.GetId(), p.GetFormat());
       }
     }
 
@@ -144,7 +144,7 @@ EvalKey<DCRTPoly> KeySwitchHYBRID::KeySwitchGenInternal(const PrivateKey<DCRTPol
     for (const auto& c : b) {
       for (const auto& p : c.GetAllElements()) {
         p.CopyValues(openfhe_cprobe_address(p.GetId()));
-        openfhe_cprobe_key(p.GetId());
+        openfhe_cprobe_key(p.GetId(), p.GetFormat());
       }
     }
 #endif
--- src/pke/lib/schemerns/rns-pke.cpp
+++ src/pke/lib/schemerns/rns-pke.cpp
@@ -58,7 +58,7 @@ Ciphertext<DCRTPoly> PKERNS::Encrypt(DCRTPoly plaintext, const PrivateKey<DCRTPo
     for (const auto& cv : ciphertext->GetElements()) {
       for (const auto& v : cv.GetAllElements()) {
         v.CopyValues(openfhe_cprobe_address(v.GetId()));
-        openfhe_cprobe_input(v.GetId());
+        openfhe_cprobe_input(v.GetId(), v.GetFormat());
       }
     }
 #endif
@@ -83,7 +83,7 @@ Ciphertext<DCRTPoly> PKERNS::Encrypt(DCRTPoly plaintext, const PublicKey<DCRTPol
     for (const auto& cv : ciphertext->GetElements()) {
       for (const auto& v : cv.GetAllElements()) {
         v.CopyValues(openfhe_cprobe_address(v.GetId()));
-        openfhe_cprobe_input(v.GetId());
+        openfhe_cprobe_input(v.GetId(), v.GetFormat());
       }
     }
 #endif
@@ -224,7 +224,7 @@ DCRTPoly PKERNS::DecryptCore(const std::vector<DCRTPoly>& cv, const PrivateKey<D
     for (const auto& v : cv) {
       for (const auto& p : v.GetAllElements()) {
         p.CopyValues(openfhe_cprobe_result(p.GetId()));
-        openfhe_cprobe_output(p.GetId());
+        openfhe_cprobe_output(p.GetId(), p.GetFormat());
       }
     }
 
